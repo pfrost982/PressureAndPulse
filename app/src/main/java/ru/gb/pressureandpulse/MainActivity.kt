89d14ea345100129f02e4a208ccb3cea49631c3f
pfrost982 <pfrost982@gmail.com>
2022-09-26 20:01:56 +0700

    Initial commit

---
+++ app/src/main/java/ru/gb/pressureandpulse/MainActivity.kt
@@ -0,0 +1,14 @@
+package ru.gb.pressureandpulse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.gb.pressureandpulse.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
